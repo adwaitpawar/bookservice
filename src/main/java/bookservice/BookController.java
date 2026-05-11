c407cdfad51b68fea486f5b9d43ad7a99c593660
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2026-05-11 14:26:21 +0530

    Update BookController.java

--- src/main/java/bookservice/BookController.java
+++ src/main/java/bookservice/BookController.java
@@ -1,6 +1,8 @@
 package bookservice;
 import java.util.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/books")
