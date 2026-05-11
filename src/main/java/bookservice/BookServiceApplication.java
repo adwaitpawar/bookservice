14605d9fcbfffbe5154239794c07bda0e4e2ddaf
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2026-05-11 13:14:18 +0530

    Update BookServiceApplication.java

--- src/main/java/bookservice/BookServiceApplication.java
+++ src/main/java/bookservice/BookServiceApplication.java
@@ -2,6 +2,7 @@ package bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,5 +11,9 @@ public class BookServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
     }
-}
 
+    @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookServiceApplication.class);
+    }
+}
